96816e90a8aea0318fbae9ede194496627d2ed4e
loomclone <lovemebench20@gmail.com>
2025-06-13 17:26:34 +0800

    customer and developer registration setup

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerDeveloper/', views.registerDeveloper, name='registerDeveloper'),
+
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('devDashboard/', views.devDashboard, name='devDashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,35 @@ from developers.forms import DeveloperForm
 from .models import User, UserProfile
 from django.contrib import messages
 from developers.models import Developer
-from django.contrib import messages
+from django.contrib import messages, auth
+from .utils import detectUser
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
 
 from django.template.defaultfilters import slugify
 
 
+# Restrict the developer from accessing the customer page
+def check_role_developer(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
+
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in!')
+        return redirect('myAccount')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             # Create the user using the form
@@ -48,7 +70,10 @@ def registerUser(request):
 
 
 def registerDeveloper(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in!')
+        return redirect('myAccount')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         dev_form = DeveloperForm(request.POST, request.FILES)
         if form.is_valid() and dev_form.is_valid:
@@ -88,3 +113,49 @@ def registerDeveloper(request):
     }
 
     return render(request, 'accounts/registerDeveloper.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in!')
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request, 'You are logged out.')
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    return render(request, 'accounts/custDashboard.html')
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_developer)
+def devDashboard(request):
+    return render(request, 'accounts/devDashboard.html')
